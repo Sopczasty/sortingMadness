838d775452d0aedd30c4801b0ff6641587ce274f
Maciej Kleban <dawnwender@gmail.com>
2021-12-19 14:22:25 +0100

    Add package info for logic

---
+++ src/main/java/pl/put/poznan/sorting/logic/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package containing all classes used for functioning of the application.
+ * <p>
+ *     This includes things such as sorting algorithms or timers.
+ * </p>
+ */
+package pl.put.poznan.sorting.logic;
