bfa591f5c66a45ec55a6e96cc34753606be5425b
Maciej Kleban <dawnwender@gmail.com>
2021-12-19 14:22:50 +0100

    Update ArraySorterController.java

--- src/main/java/pl/put/poznan/sorting/rest/ArraySorterController.java
+++ src/main/java/pl/put/poznan/sorting/rest/ArraySorterController.java
@@ -11,14 +11,24 @@ import pl.put.poznan.sorting.logic.Timer;
 import java.security.InvalidParameterException;
 import java.util.*;
 
+/**
+ * REST API Controller for simple array input
+ */
 @RestController
 @RequestMapping("/api")
 public class ArraySorterController {
+
+    // Algorithm type
     private static String algorithm;
+    // Timer instance measuring sorting time
     private static Timer timer;
+    // Input array to be sorted
     private static int[] input;
+    // Output array
     private static HashMap<String, Object> output;
+    // Wrapper returning correct sorting algorithm
     private static SortingWrapper wrapper;
+    // Sorting algorithm instance
     private static Sorter sorter;
 
     /*
@@ -28,6 +38,12 @@ public class ArraySorterController {
         -d '{"sort":"merge", "array":[1,3,4,2,8,3,2,3,54,565,4,2,1,3,4,5,3,2,3,5,1,23,3456,455,234,3456,234,345,456,345,234,123,3245,4567,456,234,346,234234,73245]}'
         localhost:8080/api/array
     */
+    /**
+     * API endpoint returning sorted array using specified method in specified order.
+     * @param payload user data
+     * @return object with sorted array and execution time
+     * @throws InvalidParameterException if the input is in incorrect format
+     */
     @RequestMapping(value = "/array", method=RequestMethod.POST)
     public @ResponseBody
     Map<String, Object> getSortedArray(@RequestBody Map<String, Object> payload)
@@ -65,6 +81,10 @@ public class ArraySorterController {
         } else throw new InvalidParameterException();
     }
 
+    /**
+     * Returns error information if the input format is incorrect.
+     * @return response object containing error
+     */
     @ExceptionHandler({InvalidParameterException.class})
     public ResponseEntity invalidParameterExceptionHandler() {
         Map<String, Object> response = new HashMap<String, Object>();
@@ -72,4 +92,3 @@ public class ArraySorterController {
         return new ResponseEntity<>(response, HttpStatus.EXPECTATION_FAILED);
     }
 }
-
