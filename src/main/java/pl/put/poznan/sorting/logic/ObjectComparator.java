e86f70ff9fd58122f42d66985318e8de812769f6
Maciej Kleban <dawnwender@gmail.com>
2021-12-19 22:24:08 +0100

    Update ObjectComparator.java

--- src/main/java/pl/put/poznan/sorting/logic/ObjectComparator.java
+++ src/main/java/pl/put/poznan/sorting/logic/ObjectComparator.java
@@ -2,14 +2,27 @@ package pl.put.poznan.sorting.logic;
 
 import java.util.Comparator;
 
+/**
+* Class used for comparing objects between each other.
+*/
 public class ObjectComparator implements Comparator<Object> {
 
     private String getter;
 
+    /**
+    * Function returning function field getter.
+    * @param field field of class to fetch
+    */
     public ObjectComparator(String field) {
         this.getter = "get" + field.substring(0, 1).toUpperCase() + field.substring(1);
     }
 
+    /**
+    * Function comparing two objects using compareTo() method.
+    * @param o1 first object
+    * @param o2 second object
+    * @return metric how first object is different from the second
+    */
     public int compare(Object o1, Object o2) {
         try {
             if (o1 != null && o2 != null) {
