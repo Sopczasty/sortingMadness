e892f85c4c7ebec67022d0fb0f8ebb12eec6d6e0
Maciej Kleban <dawnwender@gmail.com>
2021-12-19 14:21:23 +0100

    Update QuickSort.java

--- src/main/java/pl/put/poznan/sorting/logic/QuickSort.java
+++ src/main/java/pl/put/poznan/sorting/logic/QuickSort.java
@@ -1,12 +1,32 @@
 package pl.put.poznan.sorting.logic;
 
+/**
+ * Returns data sorted using quick sort algorithm.
+ */
 public class QuickSort implements Sorter {
+
+    /**
+     * Helper function swapping two elements in array
+     * @param arr input array to swap elements in
+     * @param i index of first element to swap
+     * @param j index of second element to swap
+     */
     static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
+    /**
+     * Function sorting the input subarray starting from
+     * start index up to end index
+     * based on pivot and returning a new pivot.
+     * @param arr input subarray
+     * @param low start index of the sort
+     * @param high end index of the sort
+     * @param direction direction of the sort (ascending or descending)
+     * @return new pivot
+     */
     static int partition(int[] arr, int low, int high, String direction) {
         int pivot = arr[high];
         int i = (low - 1);
@@ -39,6 +59,15 @@ public class QuickSort implements Sorter {
         return (i + 1);
     }
 
+    /**
+     * Main quick sort function sorting input array starting from
+     * start index up to end index.
+     * @param input input array to be sorted
+     * @param low start index to sort starting from
+     * @param high end index to sort up to
+     * @param direction direction of the sort (ascending or descending)
+     * @return input subarray sorted using quick sort algorithm
+     */
     static int[] quickSort(int[] input, int low, int high, String direction) {
         int pi;
         if (low < high) {
@@ -49,6 +78,12 @@ public class QuickSort implements Sorter {
         return input;
     }
 
+    /**
+     * Function invoking quick sort algorithm.
+     * @param input input array to be sorted
+     * @param direction direction of the sort (ascending or descending)
+     * @return input array sorted using quick sort algorithm
+     */
     public int[] sort(int[] input, String direction) {
 
         // Exception for empty input data
@@ -65,6 +100,12 @@ public class QuickSort implements Sorter {
         return input;
     }
 
+    /**
+     * Function invoking quick sort algorithm if the user did not
+     * provide sort direction (assuming ascending order).
+     * @param input input data to be sorted
+     * @return input data sorted using quick sort algorithm
+     */
     public int[] sort(int[] input) {
         input = sort(input, "asc");
         return input;
