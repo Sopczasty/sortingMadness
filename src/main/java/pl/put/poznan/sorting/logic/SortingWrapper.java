d84188325e4351a648e2272a4ae17fd962af5a1b
Maciej Kleban <dawnwender@gmail.com>
2021-12-19 14:21:50 +0100

    Update SortingWrapper.java

--- src/main/java/pl/put/poznan/sorting/logic/SortingWrapper.java
+++ src/main/java/pl/put/poznan/sorting/logic/SortingWrapper.java
@@ -1,6 +1,15 @@
 package pl.put.poznan.sorting.logic;
 
+/**
+ * Wrapper over sorter that returns requested sorting algorithm object.
+ */
 public class SortingWrapper {
+
+    /**
+     * Instantiate requested sorting algorithm and return it.
+     * @param type type of the algorithm to return
+     * @return object of requested sorting algorithm (or null if not found)
+     */
     public Sorter getSorter(String type) {
         if (type.equals("bubble")) return new BubbleSort();
         if (type.equals("heap")) return new HeapSort();
