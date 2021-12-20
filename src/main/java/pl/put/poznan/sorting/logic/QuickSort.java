f3a1dc164c94cbf3e5d834a2fa412d79be55be3b
Maciej Kleban <dawnwender@gmail.com>
2021-12-20 03:07:36 +0100

    Update QuickSort.java

--- src/main/java/pl/put/poznan/sorting/logic/QuickSort.java
+++ src/main/java/pl/put/poznan/sorting/logic/QuickSort.java
@@ -40,86 +40,60 @@ public class QuickSort implements Sorter {
      * Function sorting the input subarray starting from
      * start index up to end index
      * based on pivot and returning a new pivot.
-     * @param arr input subarray
+     * @param input input subarray
      * @param low start index of the sort
      * @param high end index of the sort
      * @param direction direction of the sort (ascending or descending)
      * @return new pivot
      */
-    static int partition(int[] arr, int low, int high, String direction) {
-        int pivot = arr[high];
-        int i = (low - 1);
+    static int partition(int[] input, int low, int high, String direction) {
+        int pivot = input[high];
+        int new_pivot = (low - 1);
 
-        // Sorting for ascending order
-        if(direction.equals("asc")){
-            for(int j = low; j <= high - 1; j++)
-            {
-                if (arr[j] < pivot)
-                {
-                    i++;
-                    swap(arr, i, j);
-                }
+        for(int j = low; j <= high - 1; j++) {
+            if (direction.equals("asc") && (input[j] < pivot)) {
+                new_pivot++;
+                swap(input, new_pivot, j);
             }
-            swap(arr, i + 1, high);
+            else if (direction.equals("desc") && (input[j] > pivot)) {
+                new_pivot++;
+                swap(input, new_pivot, j);
+            }
         }
+        swap(input, new_pivot + 1, high);
 
-        // Sorting for descending order
-        if(direction.equals("desc")){
-            for(int j = low; j <= high - 1; j++)
-            {
-                if (arr[j] > pivot)
-                {
-                    i++;
-                    swap(arr, i, j);
-                }
-            }
-            swap(arr, i + 1, high);
-        }
-        return (i + 1);
+        return (new_pivot + 1);
     }
 
     /**
      * Function sorting the input subarray starting from
      * start index up to end index
      * based on pivot and returning a new pivot.
-     * @param arr input subarray of objects
+     * @param input input subarray of objects
      * @param low start index of the sort
      * @param high end index of the sort
      * @param direction direction of the sort (ascending or descending)
-     * @param object attribute to sort by
+     * @param attribute object attribute to sort by
      * @return new pivot
      */
-    static int partition(ArrayList<Object> arr, int low, int high, String direction, String attribute) {
-        Object pivot = arr.get(high);
+    static int partition(ArrayList<Object> input, int low, int high, String direction, String attribute) {
+        Object pivot = input.get(high);
         ObjectComparator objectComparator = new ObjectComparator(attribute);
-        int i = (low - 1);
+        int new_pivot = (low - 1);
 
-        // Sorting for ascending order
-        if(direction.equals("asc")){
-            for(int j = low; j <= high - 1; j++)
-            {
-                if (objectComparator.compare(arr.get(j), pivot) < 0)
-                {
-                    i++;
-                    swap(arr, i, j);
-                }
+        for(int j = low; j <= high - 1; j++)
+        {
+            if (direction.equals("asc") && (objectComparator.compare(input.get(j), pivot) < 0)) {
+                new_pivot++;
+                swap(input, new_pivot, j);
             }
-            swap(arr, i + 1, high);
-        }
-
-        // Sorting for descending order
-        if(direction.equals("desc")){
-            for(int j = low; j <= high - 1; j++)
-            {
-                if (objectComparator.compare(arr.get(j), pivot) > 0)
-                {
-                    i++;
-                    swap(arr, i, j);
-                }
+            else if (direction.equals("desc") && (objectComparator.compare(input.get(j), pivot) > 0)) {
+                new_pivot++;
+                swap(input, new_pivot, j);
             }
-            swap(arr, i + 1, high);
         }
-        return (i + 1);
+        swap(input, new_pivot + 1, high);
+        return (new_pivot + 1);
     }
 
     /**
@@ -148,7 +122,7 @@ public class QuickSort implements Sorter {
      * @param low start index to sort starting from
      * @param high end index to sort up to
      * @param direction direction of the sort (ascending or descending)
-     * @param object attribute to sort by
+     * @param attribute object attribute to sort by
      * @return input subarray sorted using quick sort algorithm
      */
     static ArrayList<Object> quickSort(ArrayList<Object> input, int low, int high, String direction, String attribute) {
@@ -168,34 +142,7 @@ public class QuickSort implements Sorter {
      * @return input array sorted using quick sort algorithm
      */
     public int[] sort(int[] input, String direction) {
-
-        // Exception for empty input data
-        if(input.length == 0){
-            logger.debug("Input data is empty. Throwing exception");
-            throw new IllegalArgumentException("Input data is empty.");
-        }
-
-        // Exception for incorrect order
-        if(!direction.equals("asc") && !direction.equals("desc")){
-            logger.debug("Input order is incorrect. Throwing exception.");
-            throw new IllegalArgumentException("Input order is incorrect.");
-        }
-
-        logger.debug("Sorting for " + direction + "ending order.");
-        input = quickSort(input, 0, input.length - 1, direction);
-        return input;
-    }
-
-    /**
-     * Function invoking quick sort algorithm if the user did not
-     * provide sort direction (assuming ascending order).
-     * @param input input data to be sorted
-     * @return input data sorted using quick sort algorithm
-     */
-    public int[] sort(int[] input) {
-        logger.info("Direction undefined - assumed ascending");
-        input = sort(input, "asc");
-        return input;
+        return quickSort(input, 0, input.length - 1, direction);
     }
 
     /**
@@ -206,31 +153,9 @@ public class QuickSort implements Sorter {
      * @return input array sorted using quick sort algorithm
      */
     public ArrayList<Object> sort(ArrayList<Object> input, String direction, String attribute) {
-        // Exception for empty input data
-        if(input.size() == 0){
-            throw new IllegalArgumentException("Input data is empty.");
-        }
-
-        // Exception for incorrect order
-        if (!direction.equals("asc") && !direction.equals("desc")){
-            throw new IllegalArgumentException("Input order is incorrect.");
-        }
-        input = quickSort(input, 0, input.size() - 1, direction, attribute);
-        return input;
+        return quickSort(input, 0, input.size() - 1, direction, attribute);
     }
 
-    /**
-     * Function invoking quick sort algorithm if the user did not
-     * provide sort direction (assuming ascending order).
-     * @param input input data of objects to be sorted
-     * @return input data sorted using quick sort algorithm
-     */
-    public ArrayList<Object> sort(ArrayList<Object> input) {
-        System.out.println("Direction and parameter undefined - assumed ascending order and average time as parameter.");
-        input = sort(input, "asc", "time");
-        return input;
-    }
-  
     public String getName() {
         return "QuickSort";
     }
