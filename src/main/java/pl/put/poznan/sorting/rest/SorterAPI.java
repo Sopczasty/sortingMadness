a74386a5c2d4d21294ad66b12d234d9404c600f0
MatChmielewski <chmielewski.prod@gmail.com>
2021-12-20 14:29:36 +0100

    Iteracje poprawione 2.1 - rest

--- src/main/java/pl/put/poznan/sorting/rest/SorterAPI.java
+++ src/main/java/pl/put/poznan/sorting/rest/SorterAPI.java
@@ -1,9 +1,22 @@
 package pl.put.poznan.sorting.rest;
 
-import pl.put.poznan.sorting.logic.BubbleSort;
+//Rest imports
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.security.InvalidParameterException;
+
+/**
+ * Main API endpoint used for sortingMadness app.
+ */
+@SpringBootApplication
 public class SorterAPI {
-
+    /**
+     * Function starting the REST API
+     * @param args arguments to pass to spring app
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SorterAPI.class, args);
+    }
 }
-
-
