64adb17b97450c3ced9f08b062ad6a5e00223b62
Sopczasty <sopekloli@gmail.com>
2021-12-20 19:56:01 +0100

    Add auto detection for data structures

--- src/main/java/pl/put/poznan/sorting/logic/AutoDetector.java
+++ src/main/java/pl/put/poznan/sorting/logic/AutoDetector.java
@@ -1,15 +1,43 @@
 package pl.put.poznan.sorting.logic;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Class for detecting optimal algorithm for the specific dataset
+ */
 public class AutoDetector {
-    private int[] data = null;
+    private int[] array = null;
+    private ArrayList<Object> datastructure = null;
     private int size;
-    public AutoDetector(int[] input) throws NullPointerException {
-        data = input;
-        size = data.length;
+    private String type;
+    private String attribute;
+
+    /**
+     * Constructor for array
+     * @param input array of integers
+     * @throws NullPointerException
+     */
+    public AutoDetector(int[] input) {
+        array = input;
+        size = input.length;
+        type = "Array";
+    }
+    public AutoDetector(ArrayList<Object> input, String attribute) {
+        datastructure = input;
+        size = input.size();
+        type = "DataStructure";
+        this.attribute = attribute;
     }
 
+    /**
+     *
+     * @return a type of algorithm dependent on size and percent sorted
+     */
     public String getType() {
-        float percent = percentSorted();
+        float percent;
+        if(type.equals("Array")) percent = percentSortedArray();
+        else percent = percentSortedDataStructure();
         if(percent > 0.8 && size < 100) return "bubble";
         if(size < 100) return "selection";
         if(percent > 0.8) return "insertion";
@@ -18,10 +46,30 @@ public class AutoDetector {
         return "heap";
     }
 
-    private float percentSorted() {
+    /**
+     *
+     * @return percentage <0..1> of how much is the array sorted
+     */
+    private float percentSortedArray() {
         int sortedElements = 0;
         for(int i = 1; i < size; i++) {
-            if(data[i] > data[i-1]) {
+            if(array[i] > array[i-1]) {
+                sortedElements++;
+                if(i == (int) size/2 + 1)
+                    if((float) sortedElements/i < 0.5) return (float) sortedElements/i;
+            }
+        }
+        return (float) sortedElements/size;
+    }
+
+    /**
+     *
+     * @return percentage <0..1> of how much is the data structure sorted
+     */
+    private float percentSortedDataStructure() {
+        int sortedElements = 0;
+        for(int i = 1; i < size; i++) {
+            if(new ObjectComparator(attribute).compare(datastructure.get(i-1), datastructure.get(i)) > 0) {
                 sortedElements++;
                 if(i == (int) size/2 + 1)
                     if((float) sortedElements/i < 0.5) return (float) sortedElements/i;
