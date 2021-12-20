15d533f1c68235c07c66453a41c3b2a4acce7ab2
Maciej Kleban <dawnwender@gmail.com>
2021-12-20 03:08:05 +0100

    Update Sorter.java

--- src/main/java/pl/put/poznan/sorting/logic/Sorter.java
+++ src/main/java/pl/put/poznan/sorting/logic/Sorter.java
@@ -3,10 +3,7 @@ package pl.put.poznan.sorting.logic;
 import java.util.ArrayList;
 
 public interface Sorter {
-
     public int[] sort(int[] input, String direction);
-    public int[] sort(int[] input);
     public ArrayList<Object> sort(ArrayList<Object> input, String direction, String attribute);
-    public ArrayList<Object> sort(ArrayList<Object> input);
     public String getName();
 }
